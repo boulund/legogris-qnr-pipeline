1924280db107762c6de934a62e52c74c075f9d30
Robert Edstrom <legogris@legogris.se>
2013-06-24 15:37:37 +0200

    Some optimizations with lambda and stuff

--- codontablegen.py
+++ codontablegen.py
@@ -1,8 +1,14 @@
 #!/bin/python
+import sys
 #TODO: Loop over codon table and generate dict with strings, should be miuch faster than regex
 f = open('codons.txt', 'r')
 for l in f:
     s = l.split('"')
     (rex, acid) = s[1].split(' ')
-    print rex
+    (first, last) = rex.split('[')
+    last = last[:-1]
+    for c in last:
+        sys.stdout.write("'%s%s': '%s'," % (first, c, acid))
+    sys.stdout.write("\n")
+
 
--- readfasta.py
+++ readfasta.py
@@ -4,45 +4,59 @@ from os import path
 import os
 import select
 import time
-from Bio.Alphabet import generic_dna
-from Bio import SeqIO
 import uuid
 import json
 import subprocess, shlex
 import re
+from collections import defaultdict
 
 import berkeley
 
 _ITEM_LIMIT = 0
-_COMPLEMENTS = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
-#TODO: codontablegen.py
-_GENCODE = {
-  re.compile('TT[TCY]'): 'F',
-  re.compile('TT[AGR]'): 'L',
-  re.compile('CT.'): 'L',
-  re.compile('AT[TCAYWMH]'): 'I',
-  re.compile('ATG'): 'M',
-  re.compile('GT.'): 'V',
-  re.compile('TC.'): 'S',
-  re.compile('CC.'): 'P',
-  re.compile('AC.'): 'T',
-  re.compile('GC.'): 'A',
-  re.compile('TA[TCY]'): 'Y',
-  re.compile('TA[AGR]'): '*',
-  re.compile('CA[TCY]'): 'H',
-  re.compile('CA[AGR]'): 'Q',
-  re.compile('AA[TCY]'): 'N',
-  re.compile('AA[AGR]'): 'K',
-  re.compile('GA[TCY]'): 'D',
-  re.compile('GA[AGR]'): 'E',
-  re.compile('TG[TCY]'): 'C',
-  re.compile('TGA'): '*',
-  re.compile('TGG'): 'W',
-  re.compile('CG.'): 'R',
-  re.compile('AG[TCY]'): 'S',
-  re.compile('AG[AGR]'): 'R',
-  re.compile('GG.'): 'G'
+_COMPLEMENTS = {
+    'A': 'T',
+    'C': 'G',
+    'G': 'C',
+    'T': 'A',
+    'Y': 'R',
+    'R': 'Y',
+    'S': 'S',
+    'W': 'W',
+    'K': 'M',
+    'M': 'K',
+    'B': 'V',
+    'V': 'B',
+    'D': 'H',
+    'H': 'D',
+    'N': 'N',
+    'X': 'X',
+    '\n': ''
 }
+_GENCODE = defaultdict(lambda: 'X', {
+    'TTT': 'F','TTC': 'F','TTY': 'F',
+    'TTA': 'L','TTG': 'L','TTR': 'L',
+    'CTT': 'L','CTC': 'L','CTA': 'L','CTG': 'L','CTM': 'L','CTR': 'L','CTW': 'L','CTS': 'L','CTY': 'L','CTK': 'L','CTV': 'L','CTH': 'L','CTD': 'L','CTB': 'L','CTX': 'L','CTN': 'L',
+    'ATT': 'I','ATC': 'I','ATA': 'I','ATY': 'I','ATW': 'I','ATM': 'I','ATH': 'I', 'ATG': 'M',
+    'GTT': 'V','GTC': 'V','GTA': 'V','GTG': 'V','GTM': 'V','GTR': 'V','GTW': 'V','GTS': 'V','GTY': 'V','GTK': 'V','GTV': 'V','GTH': 'V','GTD': 'V','GTB': 'V','GTX': 'V','GTN': 'V',
+    'TCT': 'S','TCC': 'S','TCA': 'S','TCG': 'S','TCM': 'S','TCR': 'S','TCW': 'S','TCS': 'S','TCY': 'S','TCK': 'S','TCV': 'S','TCH': 'S','TCD': 'S','TCB': 'S','TCX': 'S','TCN': 'S',
+    'CCT': 'P','CCC': 'P','CCA': 'P','CCG': 'P','CCM': 'P','CCR': 'P','CCW': 'P','CCS': 'P','CCY': 'P','CCK': 'P','CCV': 'P','CCH': 'P','CCD': 'P','CCB': 'P','CCX': 'P','CCN': 'P',
+    'ACT': 'T','ACC': 'T','ACA': 'T','ACG': 'T','ACM': 'T','ACR': 'T','ACW': 'T','ACS': 'T','ACY': 'T','ACK': 'T','ACV': 'T','ACH': 'T','ACD': 'T','ACB': 'T','ACX': 'T','ACN': 'T',
+    'GCT': 'A','GCC': 'A','GCA': 'A','GCG': 'A','GCM': 'A','GCR': 'A','GCW': 'A','GCS': 'A','GCY': 'A','GCK': 'A','GCV': 'A','GCH': 'A','GCD': 'A','GCB': 'A','GCX': 'A','GCN': 'A',
+    'TAT': 'Y','TAC': 'Y','TAY': 'Y',
+    'TAA': '*','TAG': '*','TAR': '*',
+    'CAT': 'H','CAC': 'H','CAY': 'H',
+    'CAA': 'Q','CAG': 'Q','CAR': 'Q',
+    'AAT': 'N','AAC': 'N','AAY': 'N',
+    'AAA': 'K','AAG': 'K','AAR': 'K',
+    'GAT': 'D','GAC': 'D','GAY': 'D',
+    'GAA': 'E','GAG': 'E','GAR': 'E',
+    'TGT': 'C','TGC': 'C','TGY': 'C', 'TGA': '*', 'TGG': 'W',
+    'CGT': 'R','CGC': 'R','CGA': 'R','CGG': 'R','CGM': 'R','CGR': 'R','CGW': 'R','CGS': 'R','CGY': 'R','CGK': 'R','CGV': 'R','CGH': 'R','CGD': 'R','CGB': 'R','CGX': 'R','CGN': 'R',
+    'AGT': 'S','AGC': 'S','AGY': 'S',
+    'AGA': 'R','AGG': 'R','AGR': 'R',
+    'GGT': 'G','GGC': 'G','GGA': 'G','GGG': 'G','GGM': 'G','GGR': 'G','GGW': 'G','GGS': 'G','GGY': 'G','GGK': 'G','GGV': 'G','GGH': 'G','GGD': 'G','GGB': 'G','GGX': 'G','GGN': 'G',
+    'CT': 'L', 'GT': 'V', 'TC': 'S', 'CC': 'P', 'AC': 'T', 'GC': 'A', 'CG': 'R', 'GG': 'G'
+})
 
 class LineReader(object):
     def __init__(self, fd, transeq, indb, outdb):
@@ -102,11 +116,8 @@ class LineReader(object):
 #Reads FASTA file. Adds all six frames of fragments to database and a new FASTA file with new UUIDs as keys in both.
 def translate_fasta(inpath, outpath):
     outfile = open(outpath, 'w')
-    outdb = berkeley.open_fragments('n')
-    db = {} #berkeley.open_dna_input('n')
-    outdb = berkeley.open_fragments('n')
-    #p = subprocess.Popen(args, stdin=subprocess.PIPE,stdout=subprocess.PIPE,bufsize=-1)
-    #lr = LineReader(p.stdout, p.stdin, db, outdb)
+    #outdb = berkeley.open_fragments('n')
+    outdb = {} #Just for profiling/testing: No db
     infile = open(inpath,'r')
     #First step: Parse fasta file, save fragments to db and pipe to transeq process
     print('Start', time.asctime(time.localtime()))
@@ -119,89 +130,43 @@ def translate_fasta(inpath, outpath):
                 if _ITEM_LIMIT and n > _ITEM_LIMIT:
                     break
                 if len(tempseq) > 0:
-                    for frame in range(1,4):
-                        dna = _construct_sequence(seqid, ''.join(tempseq), frame, False)
-                        protein = _translate_dna(dna)
-                        seq = {
-                            'id': uuid.uuid4().hex,
-                            'dna': dna,
-                            'protein': protein,
-                            'name': seqid,
-                            'description': seqdesc.lstrip(),
-                            'frame': int(frame)
-                        }
-                        outdb[seqid] = json.dumps(seq)
-
+                    _save_sequence(seqid, seqdesc.lstrip(), ''.join(tempseq), outdb, outfile)
                 (seqid, seqdesc) = line[1::].split(' ', 1)
                 tempseq = []
             else:
                 tempseq.append(line.rstrip())
-        #TODO: Save the last one too
+        _save_sequence(seqid, seqdesc.lstrip(), ''.join(tempseq), outdb, outfile)
     except OSError:
         raise PathError(''.join(['ERROR: cannot open', refseqpath]))
     finally:
         infile.close()
         outfile.close()
-        outdb.close()
+        #outdb.close()
+        print('Finish:', time.asctime(time.localtime()))
 
-fasta_regex = re.compile('>([^\s]*_([0-6]))([^\r\n]*)\n([^>]+)')
-
-def _save_sequence(name, desc, dna, db, transeq):
-    s = ''.join(['>',name,' ', desc, '\n',dna,'\n'])
-    ids = name.rsplit('|')
-    ids.reverse()
-    id = ''
-    for _id in ids:
-        id = _id #transeq extracts id from id string, so do the same thing
-        if id != '':
-            break
-    print('Saving ', id, ' from ', name)
-    db[id] = dna
-    #transeq.stdin.write(s)
-    #transeq.stdin.flush()
-    #result = p.communicate(input=s)[0]
-    #result = p.stdout.read(-1)
-'''    for (name, frame, desc, protein) in fasta_regex.findall(result):
+def _save_sequence(name, desc, sequence, outdb, outfile):
+    #Reverse complement
+    revseq = ''.join([_COMPLEMENTS[c] for c in sequence[::-1]])
+    for frame in range(0,6):
+        if frame > 2:
+            dna = revseq[frame-3::]
+        dna = sequence[frame::]
+        #translate dna codons to protein
+        protein = ''.join([_GENCODE[dna[i:i+3]] for i in xrange(0, len(dna), 3)])
         id = uuid.uuid4().hex
         seq = {
             'id': id,
             'dna': dna,
             'protein': protein,
             'name': name,
-            'description': desc.lstrip(),
-            'frame': int(frame)
+            'description': desc,
+            'frame': frame
         }
-        #db[seq['id']] = json.dumps(seq)
+        outdb[id] = json.dumps(seq)
         out = ''.join(['>', id, '\n', protein, '\n'])
-        outfile.write(out) '''
+        outfile.write(out)
 
-def _construct_sequence(name, dna, frame=1, reverse=False):
-    if reverse:
-        dna = _reverse_complement(dna)[frame-1::]
-        frame += 3
-    else:
-        dna = dna[frame-1::]
-    return dna
+import cProfile
 
-def _translate_dna(dna):
-    pseq = []
-    for i in xrange(0, len(dna), 3):
-        codon = dna[i:i+3]
-        match = False
-        for (rex, acid) in _GENCODE.items():
-            if rex.match(codon):
-                pseq.append(acid)
-                match = True
-                break
-        if not match:
-            pseq.append('X')
-    return ''.join(pseq)
-
-def _reverse_complement(dna):
-    cs = []
-    for c in reversed(dna):
-        if c != '\n':
-            cs.append(_COMPLEMENTS[c])
-    return ''.join(cs)
-
-translate_fasta('tutorial/database/ntsmall_plus_qnr.nfa', 'test.pfa')
+cProfile.run("translate_fasta('tutorial/database/ntsmall_plus_qnr.nfa', 'test.pfa')")
+#translate_fasta('tutorial/database/ntsmall_plus_qnr.nfa', 'test.pfa')
