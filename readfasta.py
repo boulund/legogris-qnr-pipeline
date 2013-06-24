cd96185b3017a440f2dfd267c0dabba7c1ddc532
Robert Edstrom <legogris@legogris.se>
2013-06-24 21:08:16 +0200

    cython being a bit faster

--- readfasta.py
+++ readfasta.py
@@ -1,68 +1,21 @@
 #!/usr/bin/python
 from __future__ import print_function
 import time
-import uuid
-import json
-import random
-from collections import defaultdict
 
 import berkeley
+from translator import translate_sequence
 
 _DEBUG = True
-_ITEM_LIMIT = 10
-_COMPLEMENTS = {
-    'A': 'T',
-    'C': 'G',
-    'G': 'C',
-    'T': 'A',
-    'Y': 'R',
-    'R': 'Y',
-    'S': 'S',
-    'W': 'W',
-    'K': 'M',
-    'M': 'K',
-    'B': 'V',
-    'V': 'B',
-    'D': 'H',
-    'H': 'D',
-    'N': 'N',
-    'X': 'X',
-    '\n': ''
-}
-_GENCODE = defaultdict(lambda: 'X', {
-    'TTT': 'F','TTC': 'F','TTY': 'F',
-    'TTA': 'L','TTG': 'L','TTR': 'L',
-    'CTT': 'L','CTC': 'L','CTA': 'L','CTG': 'L','CTM': 'L','CTR': 'L','CTW': 'L','CTS': 'L','CTY': 'L','CTK': 'L','CTV': 'L','CTH': 'L','CTD': 'L','CTB': 'L','CTX': 'L','CTN': 'L',
-    'ATT': 'I','ATC': 'I','ATA': 'I','ATY': 'I','ATW': 'I','ATM': 'I','ATH': 'I', 'ATG': 'M',
-    'GTT': 'V','GTC': 'V','GTA': 'V','GTG': 'V','GTM': 'V','GTR': 'V','GTW': 'V','GTS': 'V','GTY': 'V','GTK': 'V','GTV': 'V','GTH': 'V','GTD': 'V','GTB': 'V','GTX': 'V','GTN': 'V',
-    'TCT': 'S','TCC': 'S','TCA': 'S','TCG': 'S','TCM': 'S','TCR': 'S','TCW': 'S','TCS': 'S','TCY': 'S','TCK': 'S','TCV': 'S','TCH': 'S','TCD': 'S','TCB': 'S','TCX': 'S','TCN': 'S',
-    'CCT': 'P','CCC': 'P','CCA': 'P','CCG': 'P','CCM': 'P','CCR': 'P','CCW': 'P','CCS': 'P','CCY': 'P','CCK': 'P','CCV': 'P','CCH': 'P','CCD': 'P','CCB': 'P','CCX': 'P','CCN': 'P',
-    'ACT': 'T','ACC': 'T','ACA': 'T','ACG': 'T','ACM': 'T','ACR': 'T','ACW': 'T','ACS': 'T','ACY': 'T','ACK': 'T','ACV': 'T','ACH': 'T','ACD': 'T','ACB': 'T','ACX': 'T','ACN': 'T',
-    'GCT': 'A','GCC': 'A','GCA': 'A','GCG': 'A','GCM': 'A','GCR': 'A','GCW': 'A','GCS': 'A','GCY': 'A','GCK': 'A','GCV': 'A','GCH': 'A','GCD': 'A','GCB': 'A','GCX': 'A','GCN': 'A',
-    'TAT': 'Y','TAC': 'Y','TAY': 'Y',
-    'TAA': '*','TAG': '*','TAR': '*',
-    'CAT': 'H','CAC': 'H','CAY': 'H',
-    'CAA': 'Q','CAG': 'Q','CAR': 'Q',
-    'AAT': 'N','AAC': 'N','AAY': 'N',
-    'AAA': 'K','AAG': 'K','AAR': 'K',
-    'GAT': 'D','GAC': 'D','GAY': 'D',
-    'GAA': 'E','GAG': 'E','GAR': 'E',
-    'TGT': 'C','TGC': 'C','TGY': 'C', 'TGA': '*', 'TGG': 'W',
-    'CGT': 'R','CGC': 'R','CGA': 'R','CGG': 'R','CGM': 'R','CGR': 'R','CGW': 'R','CGS': 'R','CGY': 'R','CGK': 'R','CGV': 'R','CGH': 'R','CGD': 'R','CGB': 'R','CGX': 'R','CGN': 'R',
-    'AGT': 'S','AGC': 'S','AGY': 'S',
-    'AGA': 'R','AGG': 'R','AGR': 'R',
-    'GGT': 'G','GGC': 'G','GGA': 'G','GGG': 'G','GGM': 'G','GGR': 'G','GGW': 'G','GGS': 'G','GGY': 'G','GGK': 'G','GGV': 'G','GGH': 'G','GGD': 'G','GGB': 'G','GGX': 'G','GGN': 'G',
-    'CT': 'L', 'GT': 'V', 'TC': 'S', 'CC': 'P', 'AC': 'T', 'GC': 'A', 'CG': 'R', 'GG': 'G'
-})
+_ITEM_LIMIT = 0
 
 #Reads FASTA file. Adds all six frames of fragments to database and a new FASTA file with new UUIDs as keys in both.
 def translate_fasta(inpath, outpath):
     outfile = open(outpath, 'w')
     infile = open(inpath,'r')
+    #outdb = berkeley.open_fragments('n')
     if _DEBUG:
         outdb = {}
         print('Start', time.asctime(time.localtime()))
-    outdb = berkeley.open_fragments('n')
     try:
         n = 0
         tempseq = []
@@ -72,7 +25,7 @@ def translate_fasta(inpath, outpath):
                 if _ITEM_LIMIT and n > _ITEM_LIMIT:
                     break
                 if len(tempseq) > 0:
-                    for (id, dump, out) in _save_sequence(seqid, seqdesc.lstrip(), ''.join(tempseq)):
+                    for (id, dump, out) in translate_sequence(seqid, seqdesc.lstrip(), ''.join(tempseq)):
                         outdb[id] = dump
                         outfile.write(out)
                 (seqid, seqdesc) = line[1::].split(' ', 1)
@@ -80,7 +33,7 @@ def translate_fasta(inpath, outpath):
             else:
                 tempseq.append(line.rstrip())
         #When the file is finished: Save the final sequence just like the others
-        for (id, dump, out) in _save_sequence(seqid, seqdesc.lstrip(), ''.join(tempseq)):
+        for (id, dump, out) in translate_sequence(seqid, seqdesc.lstrip(), ''.join(tempseq)):
             outdb[id] = dump
             outfile.write(out)
     except OSError:
@@ -90,40 +43,11 @@ def translate_fasta(inpath, outpath):
         outfile.close()
         if _DEBUG:
             print('Finish:', time.asctime(time.localtime()))
-        outdb.close()
-
-#Translates the supplied DNA string in all 6 reading frames and stores the result in a FASTA format text file as well as in serialized JSON in a supplied key/value store.
-def _save_sequence(name, desc, sequence):
-    #Local variables = less overhead
-    result = []
-    gencode = _GENCODE
-    complements = _COMPLEMENTS
-    for frame in range(0,6):
-        #First 3 frames are normal, following 3 are reverse complements
-        if frame > 2:
-            #Reverse and frame adjust
-            dna = sequence[-(frame-2)::-1]
-            #Complement
-            dna  = ''.join([complements[c] for c in dna])
         else:
-            dna  = sequence[frame::]
-        #Translate dna codons to protein
-        protein = ''.join([gencode[dna[i:i+3]] for i in xrange(0, len(dna), 3)])
-        #Faster but less secure (wrt collissions) than stock uuid4
-        id = uuid.UUID(int=random.getrandbits(128), version=4).hex
-        seq = {
-            'id': id,
-            'dna': dna,
-            'protein': protein,
-            'name': name,
-            'description': desc,
-            'frame': frame+1
-        }
-        out = ''.join(['>', id, '\n', protein, '\n'])
-        result.append((id, json.dumps(seq), out))
-    return result
+            outdb.close()
+
 
 if _DEBUG:
-    #import cProfile
-    #cProfile.run("translate_fasta('tutorial/database/ntsmall_plus_qnr.nfa', 'test.pfa')")
-    translate_fasta('tutorial/database/ntsubset_plus_7_qnr.nfa', 'test.pfa')
+    import cProfile
+    cProfile.run("translate_fasta('tutorial/database/ntsmall_plus_qnr.nfa', 'test.pfa')")
+    #translate_fasta('tutorial/database/ntsubset_plus_7_qnr.nfa', 'test.pfa')
---
+++ translator.pyx
@@ -0,0 +1,114 @@
+import uuid
+import json
+import random
+from collections import defaultdict
+#---------------------------------------------------------------------------
+cdef extern from "stdlib.h":
+  ctypedef unsigned int size_t
+  size_t strlen(char *s)
+  void *malloc(size_t size)
+  void *calloc(size_t n, size_t size)
+  void free(void *ptr)
+  int strcmp(char *a, char *b)
+  char * strcpy(char *a, char *b)
+_COMPLEMENTS = {
+    ord('A'): ord('T'),
+    ord('C'): ord('G'),
+    ord('G'): ord('C'),
+    ord('T'): ord('A'),
+    ord('Y'): ord('R'),
+    ord('R'): ord('Y'),
+    ord('S'): ord('S'),
+    ord('W'): ord('W'),
+    ord('K'): ord('M'),
+    ord('M'): ord('K'),
+    ord('B'): ord('V'),
+    ord('V'): ord('B'),
+    ord('D'): ord('H'),
+    ord('H'): ord('D'),
+    ord('N'): ord('N'),
+    ord('X'): ord('X'),
+}
+_GENCODE = defaultdict(lambda: 'X', {
+    'TTT': 'F','TTC': 'F','TTY': 'F',
+    'TTA': 'L','TTG': 'L','TTR': 'L',
+    'CTT': 'L','CTC': 'L','CTA': 'L','CTG': 'L','CTM': 'L','CTR': 'L','CTW': 'L','CTS': 'L','CTY': 'L','CTK': 'L','CTV': 'L','CTH': 'L','CTD': 'L','CTB': 'L','CTX': 'L','CTN': 'L',
+    'ATT': 'I','ATC': 'I','ATA': 'I','ATY': 'I','ATW': 'I','ATM': 'I','ATH': 'I', 'ATG': 'M',
+    'GTT': 'V','GTC': 'V','GTA': 'V','GTG': 'V','GTM': 'V','GTR': 'V','GTW': 'V','GTS': 'V','GTY': 'V','GTK': 'V','GTV': 'V','GTH': 'V','GTD': 'V','GTB': 'V','GTX': 'V','GTN': 'V',
+    'TCT': 'S','TCC': 'S','TCA': 'S','TCG': 'S','TCM': 'S','TCR': 'S','TCW': 'S','TCS': 'S','TCY': 'S','TCK': 'S','TCV': 'S','TCH': 'S','TCD': 'S','TCB': 'S','TCX': 'S','TCN': 'S',
+    'CCT': 'P','CCC': 'P','CCA': 'P','CCG': 'P','CCM': 'P','CCR': 'P','CCW': 'P','CCS': 'P','CCY': 'P','CCK': 'P','CCV': 'P','CCH': 'P','CCD': 'P','CCB': 'P','CCX': 'P','CCN': 'P',
+    'ACT': 'T','ACC': 'T','ACA': 'T','ACG': 'T','ACM': 'T','ACR': 'T','ACW': 'T','ACS': 'T','ACY': 'T','ACK': 'T','ACV': 'T','ACH': 'T','ACD': 'T','ACB': 'T','ACX': 'T','ACN': 'T',
+    'GCT': 'A','GCC': 'A','GCA': 'A','GCG': 'A','GCM': 'A','GCR': 'A','GCW': 'A','GCS': 'A','GCY': 'A','GCK': 'A','GCV': 'A','GCH': 'A','GCD': 'A','GCB': 'A','GCX': 'A','GCN': 'A',
+    'TAT': 'Y','TAC': 'Y','TAY': 'Y',
+    'TAA': '*','TAG': '*','TAR': '*',
+    'CAT': 'H','CAC': 'H','CAY': 'H',
+    'CAA': 'Q','CAG': 'Q','CAR': 'Q',
+    'AAT': 'N','AAC': 'N','AAY': 'N',
+    'AAA': 'K','AAG': 'K','AAR': 'K',
+    'GAT': 'D','GAC': 'D','GAY': 'D',
+    'GAA': 'E','GAG': 'E','GAR': 'E',
+    'TGT': 'C','TGC': 'C','TGY': 'C', 'TGA': '*', 'TGG': 'W',
+    'CGT': 'R','CGC': 'R','CGA': 'R','CGG': 'R','CGM': 'R','CGR': 'R','CGW': 'R','CGS': 'R','CGY': 'R','CGK': 'R','CGV': 'R','CGH': 'R','CGD': 'R','CGB': 'R','CGX': 'R','CGN': 'R',
+    'AGT': 'S','AGC': 'S','AGY': 'S',
+    'AGA': 'R','AGG': 'R','AGR': 'R',
+    'GGT': 'G','GGC': 'G','GGA': 'G','GGG': 'G','GGM': 'G','GGR': 'G','GGW': 'G','GGS': 'G','GGY': 'G','GGK': 'G','GGV': 'G','GGH': 'G','GGD': 'G','GGB': 'G','GGX': 'G','GGN': 'G',
+    'CT': 'L', 'GT': 'V', 'TC': 'S', 'CC': 'P', 'AC': 'T', 'GC': 'A', 'CG': 'R', 'GG': 'G'
+})
+#Translates the supplied DNA string in all 6 reading frames and stores the result in a FASTA format text file as well as in serialized JSON in a supplied key/value store.
+def translate_sequence(char *name, char *desc, char *sequence):
+    cdef int i, j, frame, l
+    cdef char *dna
+    cdef char *protein
+    cdef char c
+    cdef char* s = NULL
+    l = len(sequence)
+    cdef char* dseq = <char *>calloc(l + 1, sizeof(char))
+    #Local variables = less overhead
+    result = []
+    gencode = _GENCODE
+    complements = _COMPLEMENTS
+    for frame in range(0,6):
+        #First 3 frames are normal, following 3 are reverse complements
+        if frame > 2:
+            #Reverse and frame adjust
+            j = 0
+            for i in range(l-1, frame-4, -1):
+                c = sequence[i]
+                if c != 10: #skip newline
+                #Complement
+                    dseq[i] = complements[c]
+                    j += 1
+            #dseq[j] = 'Z'
+            #jd = ''.join(dseq)
+            dna  = dseq
+            #print('REV')
+        else:
+            #print('NOREV')
+            dd  = sequence[frame::]
+            dna = dd
+        #Translate dna codons to protein
+        pseq = []
+        for i in range(0, len(dna), 3):
+            pseq.append(gencode[dna[i:i+3]])
+        d = ''.join(pseq)
+        protein = d
+        #protein = ''.join([gencode[dna[i:i+3]] for i in xrange(0, len(dna), 3)])
+        #Faster but less secure (wrt collissions) than stock uuid4
+        id = uuid.UUID(int=random.getrandbits(128), version=4).hex
+        seq = {
+            'id': id,
+            'dna': dna,
+            'protein': protein,
+            'name': name,
+            'description': desc,
+            'frame': frame+1
+        }
+        out = ''.join(['>', id, '\n', protein, '\n'])
+        try:
+            result.append((id, json.dumps(seq), out))
+        except:
+            print("* dd: ",dd)
+            print("* dna: ", dna)
+            print(seq)
+            exit(1)
+    return result
