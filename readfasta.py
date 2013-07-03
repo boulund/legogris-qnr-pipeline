35cff6c959af403cb2194db6b1f7cf05a5bf267f
Robert Edstrom <robert.edstrom@gmail.com>
2013-07-03 10:10:46 +0200

    kyoto bulk set

--- readfasta.py
+++ readfasta.py
@@ -14,11 +14,13 @@ class FastaReader(Sieve):
     def init(self, params):
         self.outdbmode = True
         self.name = 'FASTA translator'
-        self.param_names = [('item_limit', 0), ('db_batch_size', 100000)]
+        self.param_names = [('item_limit', 0), ('db_batch_size', 1000)]
 
     def run(self, indnadb, inprotdb, infilepath, outdnadb, outprotdb, outfilepath):
         infile = open(infilepath,'r')
         outfile = open(outfilepath, 'w')
+        prots = {}
+        dnas = {}
         try:
             n = 0
             tempseq = []
@@ -31,10 +33,16 @@ class FastaReader(Sieve):
                         dna = ''.join(tempseq)
                         id = str(n)
                         seqs = translate_sequence(id, seqid, seqdesc.lstrip(), dna)
-                        outdnadb.set(id, dna)
+                        dnas[id] = dna
                         for (frame, dump, out) in seqs:
-                            outprotdb.set(''.join([id, '_', str(frame)]), dump)
+                            prots[''.join([id, '_', str(frame)])] = dump
                             outfile.write(out)
+                        if n % self.db_batch_size == 0:
+                            outdnadb.set_bulk(dnas)
+                            outprotdb.set_bulk(prots)
+                            dnas = {}
+                            prots = {}
+
                     (seqid, seqdesc) = line[1::].split(' ', 1)
                     tempseq = []
                 else:
