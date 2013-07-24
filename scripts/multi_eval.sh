915276fba421dcbe609ebf6840fc3e9212fb344a
Robert Edstrom <robert.edstrom@gmail.com>
2013-07-24 15:07:40 +0200

    some docs

--- scripts/multi_eval.sh
+++ scripts/multi_eval.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo -e "File\tLine count\tLongest line"
-echo -e "------------------------------------------------------"
+echo -e "File\tLine count\tLongest line\tAverage linge length"
+echo -e "--------------------------------------------------------------"
 for f in `ls -d */ | awk '{ print $1 }'`
 do
     file="${f}fragments_passed.nfa.result-contigs.fa"
-    echo -e "$f\t`wc -l $file | awk '{print $1}'`\t`wc -L $file | awk '{print $1}'`"
+    echo -e "$f\t`wc -l $file | awk '{print $1}'`\t`wc -L $file | awk '{print $1}'`\t`awk ' {totlen+=length($0)} END { printf("%d", totlen/NR); } ' $file`"
 done
